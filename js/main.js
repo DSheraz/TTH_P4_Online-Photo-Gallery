7ff2d94febf4284dff2ff0aff12b8457b2585666
DSheraz <36216875+DSheraz@users.noreply.github.com>
2018-03-06 21:35:13 +0800

    Add comment.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-
+//Taking list from HTML
 var $li = $('li');
 
+//Taking input from search when keyboard is pressed.
 $('#search').keyup(function(){
+  //Make input value not case-sensitive.
   const input = $(this).val().toLowerCase();
-
+  //function to loop through the list.
   $li.each(function(){
+    // The current list.
     const currentLi = $(this);
+    // The current anchor tag.
     const currentA = currentLi.children(':first');
-
+    // if else statement to hide or show the image.
     if(currentA.attr('data-title').toLowerCase().indexOf(input) === -1) {
       currentLi.hide();
     } else {
